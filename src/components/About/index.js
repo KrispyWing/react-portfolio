7c9e139901001def094df74000a3c2da6f6fdfcc
Kris Pennimpede <kpennimpede@hotmail.com>
2021-06-07 17:45:21 -0400

    updated About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ function About() {
       </div>
       <p>
         I'm Kris Pennimpede from Toronto, Ontario. I completed the Web Development Coding Bootcamp at the University of Toronto in May, 2021. 
-        Check out my projects page to see some of the projects I have completed which demonstrate my abilities to work with all aspects of the MERN stack. 
+        Check out my projects page to see some of the work I have completed which demonstrate my abilities with all aspects of the MERN stack. 
       </p>
       <p>
         Originally I am from Fort Erie, Ontario but have lived in Toronto since 2005. For the last 16 years I have worked for Livingston Intl which is one the largest custom brokers in North America. 
