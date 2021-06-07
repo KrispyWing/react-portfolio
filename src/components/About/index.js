2939b6315897584965ec94c54bd0926b33369a97
Kris Pennimpede <74478797+KrispyWing@users.noreply.github.com>
2021-06-07 17:40:33 -0400

    Update index.js
    Update About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,8 @@ function About() {
         <img className='headshot' src={headShot} alt="headshot of Kris"></img>        
       </div>
       <p>
-        I'm Kris Pennimpede from Toronto, Ontario. I am currently enrolled in the University of Toronto Web Programming Bootcamp and will graduate at the end of May.
+        I'm Kris Pennimpede from Toronto, Ontario. I completed the Web Development Coding Bootcamp at the University of Toronto in May, 2021. 
+        Check out my projects page to see some of the projects I have completed which demonstrate my abilities to work with all aspects of the MERN stack. 
       </p>
       <p>
         Originally I am from Fort Erie, Ontario but have lived in Toronto since 2005. For the last 16 years I have worked for Livingston Intl which is one the largest custom brokers in North America. 
